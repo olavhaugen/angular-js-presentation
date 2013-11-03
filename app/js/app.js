5db54707f6c71bcf13ee84eeddea8485e5c4e86f
Olav Haugen <olahau@gmail.com>
2013-11-03 20:02:19 +0100

    Edit and view directives

--- app/js/app.js
+++ app/js/app.js
@@ -22,3 +22,28 @@ angular.module('myApp').controller('ContactsCtrl', function ($scope, ContactsSer
 		$scope.contacts = contacts;
 	});
 });
+
+angular.module('myApp').controller('ContactEditCtrl', function ($scope){
+	
+});
+
+angular.module('myApp').directive('contactEdit', function () {
+	return {
+		controller: 'ContactEditCtrl',
+		templateUrl: 'templates/contact-edit.html',
+		restrict: 'EA',
+		scope: {
+			contact: '='
+		}
+	}
+});
+
+angular.module('myApp').directive('contactView', function () {
+	return {
+		templateUrl: 'templates/contact-view.html',
+		restrict: 'EA',
+		scope: {
+			contact: '='
+		}
+	}
+});

cf82703de1bd2c10d6e0679b79121d06bea36f01
Olav Haugen <olahau@gmail.com>
2013-11-03 19:50:50 +0100

    Edit contacts example with form validaiton

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,23 @@
 angular.module('myApp', ['ngRoute']).config(function ($routeProvider){
 	$routeProvider
-		.when('/', { templateUrl: 'templates/home.html', controller: 'HomeCtrl' })
-		.otherwise({ redirectTo: '/'});
+		.when('/', { 
+			templateUrl: 'templates/home.html', 
+			controller: 'HomeCtrl' 
+		})
+		.when('/contacts', { 
+			templateUrl: 'templates/contacts.html', 
+			controller: 'ContactsCtrl' 
+		})
+		.otherwise({ 
+			redirectTo: '/'
+		});
 });
 
-angular.module('myApp').controller('HomeCtrl', function ($scope, ContactsService){
+angular.module('myApp').controller('HomeCtrl', function ($scope){
 	$scope.message = "Hello world!";
+});
 
+angular.module('myApp').controller('ContactsCtrl', function ($scope, ContactsService){
 	ContactsService.getAll().then(function (contacts) {
 		$scope.contacts = contacts;
 	});
